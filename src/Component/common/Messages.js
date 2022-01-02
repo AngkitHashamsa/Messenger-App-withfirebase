a580e4b8edb0f310f32f01a8d800285248148523
Angkit <angkithashamsa21@gmail.com>
2022-01-03 00:41:36 +0530

    momemt added

--- src/Component/common/Messages.js
+++ src/Component/common/Messages.js
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import Moment from 'react-moment'
 const Messages = ({ item, chat }) => {
   let userId = localStorage.getItem('UserId')
   return (
@@ -7,12 +7,12 @@ const Messages = ({ item, chat }) => {
       {item.from === userId ? (
         <div className='flex justify-between items-center'>
           <h6 className='text-sm'>You</h6>
-          <p>{item.createdAt && item.createdAt.toDate().toDateString()}</p>
+          <Moment fromNow>{item.createdAt.toDate()}</Moment>
         </div>
       ) : (
         <div className='flex justify-between items-center'>
           <h6>{chat.name}</h6>
-          <p>{item.createdAt && item.createdAt.toDate().toDateString()}</p>
+          <Moment fromNow>{item.createdAt.toDate()}</Moment>
         </div>
       )}
       {item.media && <img src={item.media} alt='pic' />}
